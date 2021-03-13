fd77a9e83435fe663ac1b86fa8a569d6cc4e0641
Natee Singkong <natee.singkong@gmail.com>
2021-03-13 11:29:35 +0700

    Register Form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,76 +2,74 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+    <div class="card">
+        <p class="card-header-title title is-centered block">{{ __('Register') }}</p>
+        <div class="card-content block">
+            <div class="content">
+                <form method="POST" action="{{ route('register') }}">
+                    @csrf
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                    <!-- Email -->
+                    <div class="form-group row block">
+                        <label for="email">{{ __('E-Mail Address') }}</label>
+                        <input id="email" type="email" class="input @error('email') is-danger @enderror block"
+                            name="email" value="{{ old('email') }}" required autofocus placeholder="Email">
+                        @error('email')
+                        <div class="notification is-danger block">
+                            {{ $message }}
                         </div>
+                        @enderror
+                    </div>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                    <!-- Name -->
+                    <div class="form-group row block">
+                        <label for="name">{{ __('Name') }}</label>
+                        <input id="name" type="text" class="input @error('name') is-danger @enderror block" name="name"
+                            value="{{ old('name') }}" required placeholder="Mame">
+                        @error('name')
+                        <div class="notification is-danger block">
+                            {{ $message }}
                         </div>
+                        @enderror
+                    </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                    <!-- Password -->
+                    <div class="form-group row block">
+                        <label for="password">{{ __('Password') }}</label>
+                        <input id="password" type="password" class="input @error('password') is-danger @enderror block"
+                            name="password" value="{{ old('password') }}" required placeholder="Password">
+                    </div>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                    <!-- Password Confirmation -->
+                    <div class="form-group row block">
+                        <label for="password-confirm">{{ __('Password Confirmation') }}</label>
+                        <input id="password-confirm" type="password"
+                            class="input @error('password') is-danger @enderror block" name="password_confirmation"
+                            required placeholder="Password">
+                        @error('password')
+                        <div class="notification is-danger block">
+                            {{ $message }}
                         </div>
+                        @enderror
+                    </div>
 
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                    <!-- Button -->
 
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
+                    <div class="form-group row mb-0">
+                        <div class="col-md-8 offset-md-4">
+                            <button type="submit" class="button is-success">
+                                {{ __('Register') }}
+                            </button>
+
                         </div>
+                    </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                </form>
             </div>
         </div>
     </div>
 </div>
+</div>
+
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,110 +2,106 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <!-- CSRF Token -->
-  <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-  <!-- Scripts -->
-  <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
-  <!-- Fonts -->
-  <link rel="dns-prefetch" href="//fonts.gstatic.com">
-  <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-  <!-- Styles -->
-  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
 <body>
-  <nav class="navbar" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-      <a class="navbar-item" href="{{ url('/') }}">
-        <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
-      </a>
-
-      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-
-      </a>
-    </div>
-
-    <div id="navbarBasicExample" class="navbar-menu">
-      <div class="navbar-start">
-        <a class="navbar-item">
-          Home
-        </a>
-
-        <a class="navbar-item">
-          Documentation
-        </a>
-
-        <div class="navbar-item has-dropdown is-hoverable">
-          <a class="navbar-link">
-            More
-          </a>
-
-          <div class="navbar-dropdown">
-            <a class="navbar-item">
-              About
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+            <a class="navbar-item" href="{{ url('/') }}">
+                <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
             </a>
-            <a class="navbar-item">
-              Jobs
-            </a>
-            <a class="navbar-item">
-              Contact
-            </a>
-            <hr class="navbar-divider">
-            <a class="navbar-item">
-              Report an issue
-            </a>
-          </div>
+ 
+            <a class="navbar-item" href="{{ url('/home') }}">
+                    Dashboard
+                </a>
+                
         </div>
-      </div>
+        
+
+        <div id="navbarBasicExample" class="navbar-menu">
+            <div class="navbar-start">
+
+                <a class="navbar-item" href="{{ url('/home') }}">
+                    Dashboard
+                </a>
+
+                <div class="navbar-item has-dropdown is-hoverable">
+                    <a class="navbar-link">
+                        More
+                    </a>
+
+                    <div class="navbar-dropdown">
+                        <a class="navbar-item">
+                            About
+                        </a>
+                        <a class="navbar-item">
+                            Jobs
+                        </a>
+                        <a class="navbar-item">
+                            Contact
+                        </a>
+                        <hr class="navbar-divider">
+                        <a class="navbar-item">
+                            Report an issue
+                        </a>
+                    </div>
+                </div>
+            </div>
 
 
 
-      <div class="navbar-end">
-        <div class="navbar-item">
-          <div class="buttons">
-            @if (Route::has('login'))
+            <div class="navbar-end">
+                <div class="navbar-item">
+                    <div class="buttons">
+                        @if (Route::has('login'))
 
-            @auth
-            <a class="button is-primary" href="{{ url('/') }}">
-              <strong>Home</strong>
-            </a>
-            <a class="button is-danger" href="{{ url('/logout') }}">
-              <strong>Logout</strong>
-            </a>
-            @else
+                        @auth
+                        <a class="button is-primary" href="{{ url('/') }}">
+                            <strong>Home</strong>
+                        </a>
+                        <a class="button is-danger" href="{{ url('/logout') }}">
+                            <strong>Logout</strong>
+                        </a>
+                        @else
 
-            <a class="button is-primary" href="{{ route('login') }}">
-              <strong>Login</strong>
-            </a>
-            @if (Route::has('register'))
-            <a class="button" href="{{ route('register') }}">
-              <strong>Register</strong>
-            </a>
+                        <a class="button is-primary" href="{{ route('login') }}">
+                            <strong>Login</strong>
+                        </a>
+                        @if (Route::has('register'))
+                        <a class="button" href="{{ route('register') }}">
+                            <strong>Register</strong>
+                        </a>
 
-            @endif
-            @endif
+                        @endif
+                        @endif
 
-            @endif
-          </div>
+                        @endif
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </nav>
+    </nav>
 
-  <main class="py-4 bg-gray-900">
-    @yield('content')
-  </main>
+    <main class="py-4 bg-gray-900">
+        @yield('content')
+    </main>
 </body>
 
 </html>
